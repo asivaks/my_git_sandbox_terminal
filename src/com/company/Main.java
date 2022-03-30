954532c195509abdac510e607d88ff14b8c56027
Aleksey Sivaks <koshmariel@gmail.com>
2022-03-30 18:20:30 +0400

    add 3rd print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello, World");
         System.out.println("Hello, World one more time");
+        System.out.println("3rd print");
+
     }
 }
