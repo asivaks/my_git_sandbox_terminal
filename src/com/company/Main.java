9070a144febbb4d2376484c0ba0ab60c623671a6
Aleksey Sivaks <koshmariel@gmail.com>
2022-04-01 17:48:44 +0400

    making a conflict

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         System.out.println("Hello, World one more time");
         System.out.println("3rd print");
         System.out.println("4th print");
-        System.out.println("5th print for PR");
+        System.out.println("5th print for PR - creating conflict" );
 
     }
 }
